1c675eeb10701bfc74824649184d0790cc58d263
Souvik Banerjee <souvik.mlindia@gmail.com>
2021-06-12 18:58:11 +0100

    Day-3

---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/data/data.dart';
+import 'package:flutter_social_ui/screens/home_screen.dart';
+import 'package:flutter_social_ui/screens/login_screen.dart';
+
+class CustomDrawer extends StatelessWidget {
+  _buildDrawerOption(Icon icon, String title, Function onTap) {
+    return ListTile(
+      leading: icon,
+      title: Text(
+        title,
+        style: TextStyle(fontSize: 16.0, color: Colors.black),
+      ),
+      onTap: onTap,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Image(
+                image: AssetImage(currentUser.backgroundImageUrl),
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              Positioned(
+                bottom: 20.0,
+                left: 20.0,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            color: Theme.of(context).primaryColor, width: 3.0),
+                      ),
+                      child: ClipOval(
+                        child: Image(
+                          image: AssetImage(currentUser.profileImageUrl),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      currentUser.name,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                        letterSpacing: 1.5,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          _buildDrawerOption(Icon(Icons.home), "Home", () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (_) => HomeScreen()),
+            );
+          }),
+          _buildDrawerOption(Icon(Icons.chat), "Chat", () {}),
+          _buildDrawerOption(Icon(Icons.location_on), "Map", () {}),
+          _buildDrawerOption(
+              Icon(Icons.account_circle), "Your Profile details", () {}),
+          _buildDrawerOption(Icon(Icons.settings), "Settings", () {}),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: _buildDrawerOption(Icon(Icons.run_circle), "Logout", () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => LoginScreen()),
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
